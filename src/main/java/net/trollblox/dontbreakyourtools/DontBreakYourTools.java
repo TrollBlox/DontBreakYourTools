dfb2fd638493eaa622eb9534f3603f12f331fb13
I-No-oNe <novonoam76@gmail.com>
2025-01-07 20:18:05 +0200

    Improved the whole mod

--- src/main/java/net/trollblox/dontbreakyourtools/DontBreakYourTools.java
+++ src/main/java/net/trollblox/dontbreakyourtools/DontBreakYourTools.java
@@ -2,17 +2,13 @@ package net.trollblox.dontbreakyourtools;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.Mouse;
-import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.*;
-import net.minecraft.registry.Registries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DontBreakYourTools implements ModInitializer {
-	public static final String MOD_ID = "dont-break-your-tools";
 
+	public static final String MOD_ID = "dont-break-your-tools";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
@@ -22,19 +18,19 @@ public class DontBreakYourTools implements ModInitializer {
 
 	public static boolean preventAttack(ItemStack stack) {
 		int amount = 0;
-		if (stack.getItem() instanceof MiningToolItem && !(stack.getItem() instanceof SwordItem)) amount++;
+		if (stack.getItem() instanceof MiningToolItem) amount++;
 		return preventUsage(stack, amount);
 	}
 
 	public static boolean preventUsage(ItemStack stack) {
-		return (stack.isDamageable() && isDurablilityOne(stack));
+		return (stack.isDamageable() && isDurabilityOne(stack));
 	}
 
 	public static boolean preventUsage(ItemStack stack, int damagePerUse) {
 		return (stack.isDamageable() && isDurabilityOne(stack, damagePerUse));
 	}
 
-	public static boolean isDurablilityOne(ItemStack stack) {
+	public static boolean isDurabilityOne(ItemStack stack) {
 		return stack.isDamageable() && (stack.getDamage() >= stack.getMaxDamage() - 1);
 	}
 
